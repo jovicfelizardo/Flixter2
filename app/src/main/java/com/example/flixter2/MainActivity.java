e06eafaa90c3b294ab0e6d0e70ff8a1de086601b
olymp <jbiggs@bulldogs.aamu.edu>
2020-09-07 19:17:24 -0500

    Parse Movies objects

--- app/src/main/java/com/example/flixter2/MainActivity.java
+++ app/src/main/java/com/example/flixter2/MainActivity.java
@@ -12,12 +12,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
+import models.Movie;
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results" + results.toString());
+                    movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies:" + movies.size());
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit json exception", e);
                     e.printStackTrace();
